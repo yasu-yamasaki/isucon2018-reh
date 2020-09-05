9541b085c30ac227c074aa683792c91579694254
Takuya Oki <blutac.nz@gmail.com>
2020-09-05 19:44:06 +0900

    Revert "fix"

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -799,7 +799,6 @@ func main() {
 		return c.NoContent(204)
 	}, adminLoginRequired)
 	e.GET("/admin/api/events", func(c echo.Context) error {
-		ctx := newrelic.NewContext(c.Request().Context(), nrecho.FromContext(c))
 		events, err := getEvents(ctx, true)
 		if err != nil {
 			return err
